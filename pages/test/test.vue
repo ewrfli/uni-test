26bb6b87f5fab60948e034e28577a57a917ae588
ewrfli <q304062@163.com>
2021-12-20 15:57:28 +0800

    feat :add watch

--- pages/test/test.vue
+++ pages/test/test.vue
@@ -1,6 +1,30 @@
 <template>
 	<view>
+		<picker @change="bindPickerChange" :value="index" :range="array">
+			<view class="picker">
+				当前选择：{{array[index]}}
+			</view>
+		</picker>
 		
+		<radio-group class="radio-group" @change="radioChange">
+			<label class="radio" v-for="(item, index) in items" :key="item.name">
+				<radio :value="item.name" :checked="item.checked" /> {{item.value}}
+			</label>
+		</radio-group>
+		
+		<view>Original message: "{{ message }}"</view>
+		<view>计算属性 reversed message: "{{ reversedMessage }}"</view>
+		<input v-model="message" placeholder="edit me">
+		
+		 <view>fullName:{{ fullName }}</view>
+		 <button type="default" @click="buttonClick()">Click this.fullName='John Doe'</button>
+		 
+		 <view>计算属性缓存 vs 方法: "{{ methodsReversedMessage() }}"</view>
+		 
+		 <view>
+			 watch:
+			 <input type="text" v-model="word">
+		</view>
 	</view>
 </template>
 
@@ -8,8 +32,77 @@
 	export default {
 		data() {
 			return {
-				
+				word: 'word',
+				firstName: 'Foo',
+				lastName: 'Bar',
+				message: 'Hello',
+				index: 0,
+				array: ['A', 'B', 'C'],
+				items: [{
+					name: 'USA',
+					value: '美国'
+				},
+				{
+					name: 'CHN',
+					value: '中国',
+					checked: 'true'
+				},
+				{
+					name: 'BRA',
+					value: '巴西'
+				},
+				{
+					name: 'JPN',
+					value: '日本'
+				},
+				{
+					name: 'ENG',
+					value: '英国'
+				},
+				{
+					name: 'TUR',
+					value: '法国'
+				}
+			]
 			};
+		},
+		watch: {
+		            /* 使用watch来响应数据的变化 */
+			word(newVal, oldVal) {
+				console.log('最新值是：'+newVal,"原来的值是："+ oldVal);
+			}
+		},
+		computed: {
+			// 计算属性的 getter
+			reversedMessage(){
+			  return this.message.split('').reverse().join('')
+			},
+			fullName: {
+				// getter
+				get(){
+					return this.firstName + ' ' + this.lastName
+				},
+				// setter
+				set(newValue){
+					var names = newValue.split(' ')
+					this.firstName = names[0]
+					this.lastName = names[names.length - 1]
+				}
+			}
+		},
+		methods: {
+			bindPickerChange(e) {
+				console.log(e)
+			},
+			radioChange(e) {
+				console.log('radio发生change事件，携带value值为：', e.target.value)
+			},
+			buttonClick() {
+				this.fullName = 'John Doe'  //// setter
+			},
+			methodsReversedMessage(){
+				return this.message.split('').reverse().join('')
+			}
 		}
 	}
 </script>
